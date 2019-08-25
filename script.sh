0f2cf6f20b79a25c7f46f39ff0c728fa7b6eea41
Zach Lysobey <zlysobey@gmail.com>
2019-08-25 17:41:05 -0400

    commiting! (nonce=10)

--- script.sh
+++ script.sh
@@ -3,13 +3,15 @@
 git commit -m "committing!"
 
 commit_hash=$(git log "--pretty=format:%H")
-echo commit_hash=$commit_hash
+echo "commit_hash: $commit_hash"
 
 nonce=0
 while [[ $commit_hash != 0* ]]
 do
   echo "no leading zero"
   echo "nonce: $nonce"
-  git commit --amend -m "commiting! (nonce=$nonce)"
+  git commit --amend -m "commiting! (nonce=$nonce)" >/dev/null
+  commit_hash=$(git log "--pretty=format:%H")
+  echo "commit_hash: $commit_hash"
   ((nonce++))
 done
