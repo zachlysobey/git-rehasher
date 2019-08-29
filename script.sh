b098abc29e13f344a1b086c617957a369396685b
Zach Lysobey <zlysobey@gmail.com>
2019-08-29 16:20:21 -0400

    commiting! (nonce=468)

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-target_pattern=000*
+target_pattern=00*
 
 git commit -m "committing!"
 
@@ -8,7 +8,7 @@ commit_hash=$(git rev-parse HEAD)
 echo "commit_hash: $commit_hash"
 
 nonce=0
-while [[ $commit_hash != 000* ]]
+while [[ $commit_hash != target_pattern ]]
 do
   git commit --amend -m "commiting! (nonce=$nonce)" >/dev/null
   commit_hash=$(git rev-parse HEAD)
